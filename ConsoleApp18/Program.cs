690003ec7ae5cca127091a060def163830115c56
lazylizzy <petrovalizpetrova@yandex.ru>
2022-01-22 17:28:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string text = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in text)
+            {
+                switch (c)
+                {
+                    case '}':
+                        if (stack.Pop() != '{')
+                        {
+                            Console.WriteLine("Лишняя закрывающая скобка '}'");
+                        }
+                        break;
+                    case ']':
+                        if (stack.Pop() != '[')
+                        {
+                            Console.WriteLine("Лишняя закрывающая скобка ']'");
+                        }
+                        break;
+                    case ')':
+                        if (stack.Pop() != '(')
+                        {
+                            Console.WriteLine("Лишняя закрывающая скобка ')'");
+                        }
+                        break;
+                    case '{':
+                    case '[':
+                    case '(':
+                        stack.Push(c);
+                        break;
+                }
+            }
+            if (stack.Count > 0)
+            {
+                Console.WriteLine("Не найдена закрывающая скобка для " + stack.Pop());
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены верно");
+            }
+            Console.ReadKey();
+        }
+    }
+}
